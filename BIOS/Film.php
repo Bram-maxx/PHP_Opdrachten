27f6491432216932192e90977d533e5352a40392
Bram Schuitema <Schuitemabw@gmail.com>
2026-06-03 11:22:35 +0200

    First

---
+++ BIOS/Film.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+class Film
+{
+    public function __construct(
+        public readonly string $titel,
+        public readonly string $regisseur,
+        public readonly int $duurInMinuten,
+        public readonly int $leeftijdsgrens
+    ) {
+    }
+
+    public function getDuurAlsString(): string
+    {
+        $uren = intdiv($this->duurInMinuten, 60);
+        $minuten = $this->duurInMinuten % 60;
+
+        return "{$uren} uur en {$minuten} minuten";
+    }
+
+    public function isGeschiktVoor(int $leeftijd): bool
+    {
+        return $leeftijd >= $this->leeftijdsgrens;
+    }
+
+    public function getSamenvatting(): string
+    {
+        return "{$this->titel} (regisseur: {$this->regisseur}) | {$this->duurInMinuten} min | Leeftijd: {$this->leeftijdsgrens}+";
+    }
+}
